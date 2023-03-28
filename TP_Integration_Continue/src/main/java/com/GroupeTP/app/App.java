6c93c140355fcbd2aceb61f13faa1f66dad88c70
AlexandreT33 <alexandre.tellier2@gmail.com>
2023-03-28 14:07:19 +0200

    Creation projet MAVEN

---
+++ TP_Integration_Continue/src/main/java/com/GroupeTP/app/App.java
@@ -0,0 +1,13 @@
+package com.GroupeTP.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
